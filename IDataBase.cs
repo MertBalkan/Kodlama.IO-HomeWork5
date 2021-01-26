5fef6aa127e68a6d817e194cae4b043712c8a971
Mert Balkan <43827959+MertBalkan@users.noreply.github.com>
2021-01-26 12:15:19 +0300

    Abstracts

---
+++ IDataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kodlama.ioOdev5.Abstracts
+{
+    interface IDataBase
+    {
+        void RegisterPlayerInfosToDataBase(IPlayerService player);
+        void UpdatePlayerInfosFromDataBase(IPlayerService player);
+        void DeletePlayerInfosFromDataBase(IPlayerService player);
+    }
+}
---
+++ IPlayerService.cs
@@ -0,0 +1,14 @@
+﻿using Kodlama.ioOdev5.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kodlama.ioOdev5.Abstracts
+{
+    interface IPlayerService
+    {
+        void Register();
+        void Update();
+        void Delete();
+    }
+}
